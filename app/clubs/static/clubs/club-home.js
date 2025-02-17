fc45e8de3cd96b6171e3ac9e9b4dcf8ebd47b220
EvanI <eweinblatt@ufl.edu>
2025-02-17 18:01:00 -0500

    added html css and js files for club-home

---
+++ app/clubs/static/clubs/club-home.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeSections = document.querySelectorAll(".fade-section");
+  
+    const options = {
+      threshold: 0.1, 
+    };
+  
+    const observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+          obs.unobserve(entry.target);
+        }
+      });
+    }, options);
+  
+    fadeSections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
+  
+  console.log("UF Open Source Club - Smoother Gradient Page Loaded!");
+  
